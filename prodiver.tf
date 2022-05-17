2259baaceae902f2fedcee5fe4202b1210f00d8a
Manoj <101968434+manoj9885762348@users.noreply.github.com>
2022-05-17 12:15:15 +0530

    creating SG for ec2 instance

---
+++ prodiver.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+#aws_instance using amzon linux
+
+resource "aws_instance" "terraform_instance" {
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+   security_groups = [aws_security_group.TF_SG.name]
+
+  tags = {
+    Name = "jenkins"
+  }
+}
+#securitygroup using Terraform
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-03fbd33f598846400"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SGJ"
+  }
+}
+
