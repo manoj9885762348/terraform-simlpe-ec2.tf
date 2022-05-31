24cfd5ee2b9978c0ed81cda01cf89ef760d051b7
Manoj <101968434+manoj9885762348@users.noreply.github.com>
2022-05-31 04:51:27 -0700

    Ansible_installation
    ansible-installation

--- resource.tf
+++ resource.tf
@@ -1,33 +1,32 @@
-resource "aws_instance" "terraform_instance" {
-  ami           = "ami-0022f774911c1d690"
-  instance_type = "t2.micro"
-   security_groups = [aws_security_group.TF_SG.name]
-   key_name = "AWS_key"
+resource "aws_instance" "ansible_master" {
+  ami             = "ami-0022f774911c1d690"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.TG_SSH-gg.name]
+  key_name        = "AWS_key"
 
-  tags = {
-    Name = "jenkins"
-  }
- connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("AWS_key.pem")
-    host     = self.public_ip
 
-  }
-
-   provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum -y install wget",
-      "sudo amazon-linux-extras install java-openjdk11 -y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-      "sudo yum upgrade -y",
-      "sudo yum install jenkins -y",
-      "sudo systemctl enable jenkins",
-      "sudo systemctl start jenkins",
-      "sudo systemctl status jenkins",
-    ]
-   }
+  provisioner "file"{
+  source      =  "AWS_key.pem"
+  destination = "/tmp/AWS_key.pem"
 }
+  provisioner "remote-exec" {
+      inline = [
+    "sudo yum update -y",
+    "sudo yum -y install wget",
+    "sudo yum -y install python3",
+    "sudo amazon-linux-extras install ansible2 -y",
+    "ansible --version",
+    "sudo chmod 666 /tmp/AWS_key.pem"
+   ]
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("AWS_key.pem")
+  }
+  tags = {
+    "Name" = "ansible_Master"
+  }
 
+}
