7dc6a302c44b999381708d4317b68fbd5f63ed90
Manoj <101968434+manoj9885762348@users.noreply.github.com>
2022-07-25 10:33:00 +0530

    Creating a Ansible-Master
    Creating a Ansible-Master using terraform

---
+++ Ansible-file/ansible.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "ansible_master" {
+  ami             = "ami-0022f774911c1d690"       ##ami ID
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.TG_SSH-gg.name]  ###security group name
+  key_name        = "AWS_key"
+
+
+  provisioner "file"{
+  source      =  "AWS_key.pem"   ###here we need to submite our pem-key
+  destination = "/tmp/AWS_key.pem"
+}
+  provisioner "remote-exec" {
+      inline = [
+    "sudo yum update -y",
+    "sudo yum -y install wget",
+    "sudo yum -y install python3",
+    "sudo amazon-linux-extras install ansible2 -y",
+    "ansible --version",
+    "sudo chmod 666 /tmp/AWS_key.pem"
+   ]
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("AWS_key.pem")
+  }
+  tags = {
+    "Name" = "ansible_Master"
+  }
+
+}
